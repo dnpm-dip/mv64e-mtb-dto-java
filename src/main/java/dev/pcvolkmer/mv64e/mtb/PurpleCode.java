3770294e7b89caff7187549ad671df4c05cb6ff8
Paul-Christian Volkmer <code@pcvolkmer.de>
2024-07-10 12:44:51 +0200

    Rename purple code enum items

--- src/main/java/dev/pcvolkmer/mv64e/mtb/PurpleCode.java
+++ src/main/java/dev/pcvolkmer/mv64e/mtb/PurpleCode.java
@@ -4,16 +4,16 @@ import java.io.IOException;
 import com.fasterxml.jackson.annotation.*;
 
 public enum PurpleCode {
-    THE_0, THE_1, THE_2, THE_3, THE_4;
+    CODE_0, CODE_1, CODE_2, CODE_3, CODE_4;
 
     @JsonValue
     public String toValue() {
         switch (this) {
-            case THE_0: return "0";
-            case THE_1: return "1";
-            case THE_2: return "2";
-            case THE_3: return "3";
-            case THE_4: return "4";
+            case CODE_0: return "0";
+            case CODE_1: return "1";
+            case CODE_2: return "2";
+            case CODE_3: return "3";
+            case CODE_4: return "4";
         }
         return null;
     }
@@ -22,15 +22,15 @@ public enum PurpleCode {
     public static PurpleCode forValue(String value) throws IOException {
         switch (value) {
             case "0":
-                return THE_0;
+                return CODE_0;
             case "1":
-                return THE_1;
+                return CODE_1;
             case "2":
-                return THE_2;
+                return CODE_2;
             case "3":
-                return THE_3;
+                return CODE_3;
             case "4":
-                return THE_4;
+                return CODE_4;
         }
         throw new IOException("Cannot deserialize PurpleCode");
     }
