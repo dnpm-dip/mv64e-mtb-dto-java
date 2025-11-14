77f3699c79f84f81cf0a94f3e69660f303f1b19b
Paul-Christian Volkmer <code@pcvolkmer.de>
2025-11-14 13:29:52 +0100

    feat: replace research consent Map with custom type (#27)

--- src/main/java/dev/pcvolkmer/mv64e/mtb/MvhMetadata.java
+++ src/main/java/dev/pcvolkmer/mv64e/mtb/MvhMetadata.java
@@ -1,10 +1,14 @@
 package dev.pcvolkmer.mv64e.mtb;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.*;
 
 import java.util.List;
-import java.util.Map;
+import java.util.TreeMap;
 
 @Data
 @AllArgsConstructor
@@ -16,7 +20,7 @@ public class MvhMetadata {
     private ModelProjectConsent modelProjectConsent;
     @Getter(onMethod_ = {@JsonProperty("researchConsents")})
     @Setter(onMethod_ = {@JsonProperty("researchConsents")})
-    private List<Map<String, Object>> researchConsents;
+    private List<ResearchConsent> researchConsents;
     @Getter(onMethod_ = {@JsonProperty("reasonResearchConsentMissing")})
     @Setter(onMethod_ = {@JsonProperty("reasonResearchConsentMissing")})
     private ResearchConsentReasonMissing reasonResearchConsentMissing;
@@ -26,4 +30,28 @@ public class MvhMetadata {
     @Getter(onMethod_ = {@JsonProperty("type")})
     @Setter(onMethod_ = {@JsonProperty("type")})
     private MvhSubmissionType type;
+
+    public static class ResearchConsent extends TreeMap<String, Object> {
+
+        /**
+         * Creates a research consent from ObjectNode. As a developer, you are responsible to
+         * use a FHIR R4 Consent JSON-Objekt.
+         * @param researchConsents The FHIR R4 Consent JSON-Objekt
+         * @return the research consent object
+         */
+        public static ResearchConsent from(ObjectNode researchConsents) {
+            if (null == researchConsents) {
+                throw new IllegalArgumentException("researchConsent cannot be null");
+            }
+
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.convertValue(researchConsents, new TypeReference<>() {});
+        }
+
+        public ObjectNode asObjectNode() {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.convertValue(this, ObjectNode.class);
+        }
+    }
+
 }
